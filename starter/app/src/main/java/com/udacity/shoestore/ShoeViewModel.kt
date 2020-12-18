afb14b0ec9131f03ba9da66b81806ef0fac357ab
David Steury <david.steury@gmail.com>
2020-12-18 07:23:25 -0600

    Put in changes through step 14, except need to make save button add new shoe to shoe view model.

--- starter/app/src/main/java/com/udacity/shoestore/ShoeViewModel.kt
+++ starter/app/src/main/java/com/udacity/shoestore/ShoeViewModel.kt
@@ -9,22 +9,7 @@ class ShoeViewModel : ViewModel() {
 
     private val dummyList = mutableListOf<Shoe>(
         Shoe("Keyano", 12.0, "Asics", "Running Shoe"),
-        Shoe("Keyano", 11.5, "Asics", "Running Shoe"),
-        Shoe("Keyano", 11.0, "Asics", "Running Shoe"),
-        Shoe("Keyano", 10.5, "Asics", "Running Shoe"),
-        Shoe("Keyano", 10.0, "Asics", "Running Shoe"),
-        Shoe("Keyano", 9.5, "Asics", "Running Shoe"),
-        Shoe("Keyano", 9.0, "Asics", "Running Shoe"),
-        Shoe("Keyano", 8.5, "Asics", "Running Shoe"),
-        Shoe("Keyano", 8.0, "Asics", "Running Shoe"),
-        Shoe("Keyano", 7.5, "Asics", "Running Shoe"),
-        Shoe("Keyano", 7.0, "Asics", "Running Shoe"),
-        Shoe("Keyano", 6.5, "Asics", "Running Shoe"),
-        Shoe("Keyano", 6.0, "Asics", "Running Shoe"),
-        Shoe("Keyano", 5.5, "Asics", "Running Shoe"),
-        Shoe("Keyano", 5.0, "Asics", "Running Shoe"),
-        Shoe("Keyano", 4.5, "Asics", "Running Shoe"),
-        Shoe("Keyano", 4.0, "Asics", "Running Shoe")
+        Shoe("Keyano", 11.5, "Asics", "Running Shoe")
     )
 
     private val _shoeList = MutableLiveData<List<Shoe>>(dummyList)
--- starter/app/src/main/java/com/udacity/shoestore/shoe_detail/ShoeDetailFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/shoe_detail/ShoeDetailFragment.kt
@@ -6,11 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.R
+import com.udacity.shoestore.ShoeViewModel
 import com.udacity.shoestore.databinding.FragmentShoeDetailBinding
+import com.udacity.shoestore.shoe_list.ShoeListFragmentDirections
 
 class ShoeDetailFragment : Fragment() {
 
+    private val shoesViewModel: ShoeViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,6 +23,15 @@ class ShoeDetailFragment : Fragment() {
     ): View {
         val binding: FragmentShoeDetailBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_shoe_detail, container, false)
+
+        binding.cancelShoeButton.setOnClickListener{
+            findNavController().navigateUp()
+        }
+
+        binding.saveShoeButton.setOnClickListener {
+            findNavController().navigateUp()
+        }
+
         return binding.root
     }
 
